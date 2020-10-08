52d74963dc014b2f5542deb3bd7eeb067898d6cd
Victor <ParenVic@i.ua>
2020-10-08 11:56:24 +0300

    markup tweet page and SideMenu.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,154 +1,148 @@
+import { Container,  createStyles,  Grid,  InputBase, makeStyles, Paper, Theme, Typography, withStyles, } from '@material-ui/core';
 import React from 'react';
-import {Avatar, Container, Grid, IconButton, List, ListItem, Paper, TextField, Typography} from '@material-ui/core';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import HomeIcon from '@material-ui/icons/Home';
-import SearchIcon from '@material-ui/icons/Search';
-import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import ListIcon from '@material-ui/icons/List';
-import PermIdentityIcon from '@material-ui/icons/PermIdentity';
-import {makeStyles} from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import { SideMenu } from '../components/SideMenu';
+import {Tweet} from '../components/tweet';
 
-
-export const useHomeStyles = makeStyles((theme) => ({
+export const useHomeStyles = makeStyles((theme: Theme) => ({
     wrapper: {
-        height: '100vh'
+        height: '100vh',
     },
     logo: {
-        fontSize: 40,
         margin: '10px 0',
     },
     logoIcon: {
-        fontsize: 36
+        fontSize: 36,
     },
     sideMenuList: {
         listStyle: 'none',
         padding: 0,
         margin: 0,
+        width: 230,
     },
     sideMenuListItem: {
-        display: 'flex',
-        alignItems: 'center',
+        cursor: 'pointer',
+        '&:hover': {
+            '& div': {
+                backgroundColor: 'rgba(29, 161, 242, 0.1)',
+                '& h6': {
+                    color: theme.palette.primary.main,
+                },
+                '& svg path': {
+                    fill: theme.palette.primary.main,
+                },
+            },
+        },
 
+        '& div': {
+            display: 'inline-flex',
+            alignItems: 'center',
+            position: 'relative',
+            padding: '0 25px 0 20px',
+            borderRadius: 30,
+            height: 50,
+            marginBottom: 15,
+            transition: 'background-color 0.1s ease-in-out',
+        },
     },
-    sideMenuListLabel: {
+    sideMenuListItemLabel: {
         fontWeight: 700,
         fontSize: 20,
         marginLeft: 15,
     },
     sideMenuListItemIcon: {
-        fontSize: 30,
+        fontSize: 32,
+        marginLeft: -5,
+    },
+    sideMenuTweetButton: {
+        padding: theme.spacing(3.2),
+        marginTop: theme.spacing(2),
     },
     tweetsWrapper: {
         borderRadius: 0,
         height: '100%',
-        borderTop: 0,
-        borderBottom: 0
+        borderTop: '0',
+        borderBottom: '0',
     },
     tweetsHeader: {
-        borderTop: 0,
-        borderLeft: 0,
-        borderRight: 0,
+        borderTop: '0',
+        borderLeft: '0',
+        borderRight: '0',
         borderRadius: 0,
-        padding: '20px 15px',
+        padding: '10px 15px',
         '& h6': {
-            fontWeight: 800
-        }
+            fontWeight: 800,
+        },
     },
+    tweet: {
+        cursor: 'pointer',
+        paddingTop: 15,
+        paddingLeft: 20,
+        '&:hover': {
+            backgroundColor: 'rgb(245, 248, 250)',
+        },
+    },
+    tweetAvatar: {
+        width: theme.spacing(5),
+        height: theme.spacing(5),
+    },
+    tweetFooter: {
+        display: 'flex',
+        position: 'relative',
+        left: -13,
+        justifyContent: 'space-between',
+        width: 450,
+    },
+    tweetUserName: {
+        color: grey[500],
+    },
+}));
 
-    // hover: {
-    //     '&:hover': {
-    //         color: theme.palette.primary.main,
-    //         // color: 'green'
-    //     },
-// },
-}))
-
+const SearchTextField = withStyles(() =>
+    createStyles({
+        input: {
+            borderRadius: 30,
+            backgroundColor: '#E6ECF0',
+            height: 45,
+            padding: 0,
+        },
+    }),
+)(InputBase);
 
 export const Home = () => {
-
     const classes = useHomeStyles();
 
     return (
-        <Container maxWidth={'md'} className={classes.wrapper}>
+        <Container className={classes.wrapper} maxWidth="lg">
             <Grid container spacing={3}>
                 <Grid item xs={3}>
-                    <ul className={classes.sideMenuList}>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton className={classes.logo} size="small">
-                                <TwitterIcon color="primary" className={classes.logoIcon}/>
-                            </IconButton>
-                        </li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <HomeIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton></li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <SearchIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton>
-                            <Typography className={classes.sideMenuListLabel} variant='h6'>Поиск</Typography>
-                        </li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <NotificationsNoneIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton>
-                            <Typography className={classes.sideMenuListLabel} variant='h6'>Уведомления</Typography>
-                        </li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <MailOutlineIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton>
-                            <Typography className={classes.sideMenuListLabel} variant='h6'>Сообщения</Typography>
-                        </li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <BookmarkBorderIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton>
-                            <Typography className={classes.sideMenuListLabel} variant='h6'>Закладки</Typography>
-                        </li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <ListIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton>
-                            <Typography className={classes.sideMenuListLabel} variant='h6'>Список</Typography>
-                        </li>
-                        <li className={classes.sideMenuListItem}>
-                            <IconButton size="small">
-                                <PermIdentityIcon className={classes.sideMenuListItemIcon}/>
-                            </IconButton>
-                            <Typography className={classes.sideMenuListLabel} variant='h6'>Профиль</Typography>
-                        </li>
-                    </ul>
-
+                    <SideMenu classes={classes} />
                 </Grid>
                 <Grid item xs={6}>
-                    <Paper className={classes.tweetsWrapper} variant='outlined'>
-                        <Paper className={classes.tweetsHeader} variant='outlined'><Typography
-                            variant='h6'>Главная</Typography></Paper>
-                    </Paper>
-                    <Paper className={classes.tweetsWrapper} variant='outlined'>
-                        <Grid container spacing={3}>
-                            <Grid item xs={1}>
-                                <Avatar alt="User Avatar"
-                                        src="https://images.unsplash.com/photo-1557555187-23d685287bc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"/>
-                            </Grid>
-                            <Grid item xs={11}>
-                           <b>Lola</b>      @Lola
-
-                            </Grid>
-                        </Grid>
+                    <Paper className={classes.tweetsWrapper} variant="outlined">
+                        <Paper className={classes.tweetsHeader} variant="outlined">
+                            <Typography variant="h6">Главная</Typography>
+                        </Paper>
+                        {[
+                            ...new Array(20).fill(
+                                <Tweet
+                                    text="Петиция чтобы в каждой пачке сухариков всегда лежал один большой в три слоя обсыпанный химическими специями царь-сухарик."
+                                    user={{
+                                        fullname: 'Glafira Zhur',
+                                        username: 'GlafiraZhur',
+                                        avatarUrl:
+                                            'https://images.unsplash.com/photo-1528914457842-1af67b57139d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+                                    }}
+                                    classes={classes}
+                                />,
+                            ),
+                        ]}
                     </Paper>
                 </Grid>
                 <Grid item xs={3}>
-                    <TextField id="filled-basic" label="Поиск в Твиттере" variant="filled" fullWidth>
-                        <IconButton>
-                            <SearchIcon/>
-                        </IconButton>
-                    </TextField>
+                    <SearchTextField placeholder="Поиск по Твиттеру" fullWidth />
                 </Grid>
             </Grid>
         </Container>
-    )
-}
+    );
+};
