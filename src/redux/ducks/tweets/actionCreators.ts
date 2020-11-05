993fb75104bf925538ac914de181c03d7c2b05ec
Victor <ParenVic@i.ua>
2020-11-05 18:44:17 +0200

    success add Tweet from postman

--- src/redux/ducks/tweets/actionCreators.ts
+++ src/redux/ducks/tweets/actionCreators.ts
@@ -1,18 +1,24 @@
 import {Action} from 'redux';
-import {LoadingState, TweetsState} from './contracts/state';
+import {LoadingState, TweetsState, Tweet} from './contracts/state';
 
 export enum TweetsActionsType {
     SET_TWEETS = 'tweets/SET_TWEETS',
     FETCH_TWEETS = 'tweets/FETCH_TWEETS',
-    SET_LOADIND_STATE = 'tweets/SET_LOADIND_STATE'
+    SET_LOADIND_STATE = 'tweets/SET_LOADIND_STATE',
+    ADD_TWEET = 'tweets/ADD_TWEET'
 }
 export type SetTweetsActionType  = Action<TweetsActionsType> &  {
     type: TweetsActionsType.SET_TWEETS;
     payload: TweetsState['items'];
 }
+
+export type AddTweetActionType  = Action<TweetsActionsType> &  {
+    type: TweetsActionsType.ADD_TWEET;
+    payload: string;
+}
+
 export type FetchTweetsActionType  = Action<TweetsActionsType> &  {
     type: TweetsActionsType.FETCH_TWEETS;
-
 }
 
 
@@ -32,7 +38,10 @@ export const setTweets = (payload: TweetsState['items']): SetTweetsActionType =>
     payload,
 });
 
-
+export const addTweet = (payload: string): AddTweetActionType => ({
+    type: TweetsActionsType.ADD_TWEET,
+    payload,
+});
 
 export const fetchTweets = (): FetchTweetsActionType => ({
     type: TweetsActionsType.FETCH_TWEETS,
--- src/redux/ducks/tweets/saga.ts
+++ src/redux/ducks/tweets/saga.ts
@@ -1,7 +1,7 @@
 import { takeEvery, call, put } from 'redux-saga/effects'
 import {setTweets, setTweetsLoadingState, TweetsActionsType} from './actionCreators';
 
-import {LoadingState} from './contracts/state';
+import {LoadingState, Tweet} from './contracts/state';
 import { TweetsApi } from '../../../services/api/tweetsApi';
 
 
@@ -12,12 +12,23 @@ export function* fetchTweetsRequest() {
     try {
         const items = yield call(TweetsApi.fetchTweets);
         yield put(setTweets(items));
-        console.log(items)
     }
     catch (e) {
         yield put(setTweetsLoadingState(LoadingState.ERROR))
     }
 }
+
+// export function* addTweetsRequest(payload: Tweet) {
+//     try {
+//         const items = yield call(TweetsApi.addTweet, payload);
+//         yield put(addTweet(items));
+//         console.log(items)
+//     }
+//     catch (e) {
+//         yield put(setTweetsLoadingState(LoadingState.ERROR))
+//     }
+// }
+
 
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 export function* tweetsSaga() {
